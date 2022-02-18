d94325060fff0350a33f9db6e3ffbe4346e5a089
whatwant <whatwant@gmail.com>
2022-02-19 03:35:44 +0900

    5주차 실습 내용 추가

---
+++ 05-week/arguments/fortuneloop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+trap "exit" SIGINT
+
+INTERVAL=$1
+echo generate every $INTERVAL seconds
+mkdir -p /var/htdocs
+
+while :
+do
+  echo $(date) Writing to /var/htdocs/index.html
+  /usr/games/fortune > /var/htdocs/index.html
+  sleep $INTERVAL
+done
