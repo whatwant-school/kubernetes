3dfcf54cc75c3286fa55158ab9092aff9697687f
whatwant <whatwant@gmail.com>
2022-08-20 10:10:31 +0900

    05-Ingress-LoadBalancer 실습자료 등록

---
+++ 05-Ingress-LoadBalancer/hands-on/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const os = require('os');
+
+console.log("node-web server starting...");
+
+var handler = function(request, response) {
+  console.log("Received request from " + request.connection.remoteAddress);
+  response.writeHead(200);
+  response.end("You've hit " + os.hostname() + "(Ver2.0)\n");
+};
+
+var www = http.createServer(handler);
+www.listen(8080);
